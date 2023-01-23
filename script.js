0c44424b10308ea4b654d18d3261f9d8b72f41c5
Philip Truong <philtr928@gmail.com>
2023-01-23 02:36:50 -0800

    animations and mobile responsiveness

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function copyToClipboard(text) {
   navigator.clipboard.writeText(text);
 
-  const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+  const alertPlaceholder = document.querySelector('#alertPlaceholder')
   const wrapper = document.createElement('div')
   wrapper.innerHTML = [
-    `<div class="alert alert-secondary alert-dismissible" role="alert">`,
-    `   <div>Copied email address to clipboard</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    `<div id="copy-alert" style="display: block;">`,
+    `   <div class="alert-text">Copied email address to clipboard!</div>`,
     '</div>'
   ].join('')
 
   alertPlaceholder.append(wrapper)
+
+  setTimeout(()=> document.querySelector('#copy-alert').style.display = 'none', 2500);
+}
+
+function toggleMenu() {
+  const hamburger = document.querySelector("#hamburger")
+  hamburger.style.display = 'none'
+  const navbar = document.querySelector("#navbarNav")
+  navbar.style.display = 'block!important'
 }
